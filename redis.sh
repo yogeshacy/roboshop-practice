121cdac6173e2434056aff1f897a575ac107a2dc
YOGESH A C <yogeshacy@outlook.com>
2023-10-29 15:53:03 +0530

    updating ./roboshop-practice

--- redis.sh
+++ redis.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-set -e
+#set -e
+source common.sh
+COMPONENT=redis
 
-curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
-yum install redis-6.2.13 -y
+echo setup yum repo
+curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${LOG}
+StatusCheck
 
-systemctl enable redis
-systemctl start redis
+echo installing redis
+yum install redis-6.2.13 -y &>>${LOG}
+StatusCheck
 
+echo start redis
+systemctl enable redis &>>${LOG} && systemctl start redis &>>${LOG}
+StatusCheck
