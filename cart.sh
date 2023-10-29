644bba017df344d6e50d037008bc4a4784eaf978
YOGESH A C <yogeshacy@outlook.com>
2023-10-29 13:11:03 +0530

    updating ./roboshop-practice

--- cart.sh
+++ cart.sh
@@ -1,87 +1,50 @@
 #!/bin/bash
 
-echo Setting NodeJS repo
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>> /tmp/cart.
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSuccess\e[0m"
-else
-  echo -e "\e[31mFailed\e[0m"
-  exit 1
-fi
-
-echo Installing NodeJS
-yum install nodejs -y &>> /tmp/cart.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSuccess\e[0m"
-else
-  echo -e "\e[31mFailed\e[0m"
-  exit 1
-fi
-
-id roboshop &>> /tmp/cart.log
-if [ $? -ne 0 ]; then
-  echo adding application user
-  useradd roboshop &>> /tmp/cart.log
+StatusCheck() {
   if [ $? -eq 0 ]; then
     echo -e "\e[32mSuccess\e[0m"
   else
     echo -e "\e[31mFailed\e[0m"
     exit 1
   fi
+}
+echo Setting NodeJS repo
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>> /tmp/cart.
+StatusCheck
+
+echo Installing NodeJS
+yum install nodejs -y &>> /tmp/cart.log
+StatusCheck
+
+id roboshop &>> /tmp/cart.log
+if [ $? -ne 0 ]; then
+  echo adding application user
+  useradd roboshop &>> /tmp/cart.log
+  StatusCheck
 fi
 
 echo Downloading app content
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>> /tmp/cart.log
 cd /home/roboshop &>> /tmp/cart.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSuccess\e[0m"
-else
-  echo -e "\e[31mFailed\e[0m"
-  exit 1
-fi
+StatusCheck
 
 echo Cleaning old app content
 rm -rf cart &>> /tmp/cart.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSuccess\e[0m"
-else
-  echo -e "\e[31mFailed\e[0m"
-  exit 1
-fi
+StatusCheck
 
 echo Extracting App archive
 unzip -o /tmp/cart.zip &>> /tmp/cart.log && mv cart-main cart &>> /tmp/cart.log && cd cart &>> /tmp/cart.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSuccess\e[0m"
-else
-  echo -e "\e[31mFailed\e[0m"
-  exit 1
-fi
+StatusCheck
 
 echo Installing NodeJS  Dependencies
 npm install &>> /tmp/cart.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSuccess\e[0m"
-else
-  echo -e "\e[31mFailed\e[0m"
-  exit 1
-fi
+StatusCheck
 
 echo Config the systemD service
 mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>> /tmp/cart.log && systemctl daemon-reload &>> /tmp/cart.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSuccess\e[0m"
-else
-  echo -e "\e[31mFailed\e[0m"
-  exit 1
-fi
+StatusCheck
 
 echo Startng Cart servcie
 systemctl start cart &>> /tmp/cart.log && systemctl enable cart &>> /tmp/cart.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSuccess\e[0m"
-else
-  echo -e "\e[31mFailed\e[0m"
-  exit 1
-fi
+StatusCheck
 
