57e6f01735428257d641a323bb169d0ca4e084dc
YOGESH A C <yogeshacy@outlook.com>
2023-10-29 18:58:01 +0530

    updating ./roboshop-practice

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 #set -e
-source common.sh
 COMPONENT=rabbitmq
+source common.sh
 
-if [ -z "$APP_RABBITMQ_PASSWORD"]; then
-  echo -e "\e[33m env app password needed \e[0m"
-  exit 1
+if [ -z "$APP_RABBITMQ_PASSWORD" ]; then
+   echo -e "\e[33m env app password needed \e[0m"
+   exit 1
 fi
+
 echo Setup yum repo
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash &>>${LOG}
 StatusCheck
