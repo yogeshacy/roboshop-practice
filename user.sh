72dcadbc4f497555f0e6d6beb3d9ae01259f3ee3
YOGESH A C <yogeshacy@outlook.com>
2023-10-29 13:36:09 +0530

    updating ./roboshop-practice

--- user.sh
+++ user.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-set -e
-
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
-yum install nodejs -y
-
-useradd roboshop
-
-curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
-cd /home/roboshop
-rm -rf user
-unzip -o /tmp/user.zip
-mv user-main user
-cd /home/roboshop/user
-npm install
-
-mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
-systemctl daemon-reload
-systemctl start user
-systemctl enable user
+source common.sh
+COMPONENT=user
+NODEJS
 
